abee9c6d7032bc62e8521cd7cd539e5c172b329e
James <1536755789@qq.com>
2022-08-09 09:38:37 +0800

    init2

---
+++ src/components/Greeter.tsx
@@ -0,0 +1,218 @@
+import { useWeb3React } from '@web3-react/core';
+import { Contract, ethers, Signer } from 'ethers';
+import {
+  ChangeEvent,
+  MouseEvent,
+  ReactElement,
+  useEffect,
+  useState
+} from 'react';
+import styled from 'styled-components';
+import Proxy from '../artifacts/contracts/Proxy.sol/Proxy.json';
+import { Provider } from '../utils/provider';
+import { SectionDivider } from './SectionDivider';
+
+const StyledDeployContractButton = styled.button`
+  width: 180px;
+  height: 2rem;
+  border-radius: 1rem;
+  border-color: blue;
+  cursor: pointer;
+  place-self: center;
+`;
+
+const StyledGreetingDiv = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: 135px 2.7fr 1fr;
+  grid-gap: 10px;
+  place-self: center;
+  align-items: center;
+`;
+
+const StyledLabel = styled.label`
+  font-weight: bold;
+`;
+
+const StyledInput = styled.input`
+  padding: 0.4rem 0.6rem;
+  line-height: 2fr;
+`;
+
+const StyledButton = styled.button`
+  width: 150px;
+  height: 2rem;
+  border-radius: 1rem;
+  border-color: blue;
+  cursor: pointer;
+`;
+
+export function Greeter(): ReactElement {
+  const context = useWeb3React<Provider>();
+  const { library, active } = context;
+
+  const [signer, setSigner] = useState<Signer>();
+  const [greeterContract, setGreeterContract] = useState<Contract>();
+  const [greeterContractAddr, setGreeterContractAddr] = useState<string>('');
+  const [greeting, setGreeting] = useState<string>('');
+  const [greetingInput, setGreetingInput] = useState<string>('');
+
+  useEffect((): void => {
+    if (!library) {
+      setSigner(undefined);
+      return;
+    }
+
+    setSigner(library.getSigner());
+  }, [library]);
+
+  useEffect((): void => {
+    if (!greeterContract) {
+      return;
+    }
+
+    async function getGreeting(greeterContract: Contract): Promise<void> {
+      console.log(greeterContract.logicContractAddress)
+      const _greeting = await greeterContract.logicContractAddress();
+
+      if (_greeting !== greeting) {
+        setGreeting(_greeting);
+      }
+    }
+
+    getGreeting(greeterContract);
+  }, [greeterContract, greeting]);
+
+  function handleDeployContract(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    // only deploy the Greeter contract one time, when a signer is defined
+    if (greeterContract || !signer) {
+      return;
+    }
+
+    async function deployGreeterContract(signer: Signer): Promise<void> {
+      const Greeter = new ethers.ContractFactory(
+        Proxy.abi,
+        Proxy.bytecode,
+        signer
+      );
+
+      try {
+        const wbnbTestAddress = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
+        const myNFTAddress = "0x5E11B8EC2e5BB3387D28C77e16037A9AA6e70a85"
+        const myNFTTokenId = 777;
+
+        const greeterContract = await Greeter.deploy(wbnbTestAddress, myNFTAddress, myNFTTokenId, 200, 100, 5);
+
+        await greeterContract.deployed();
+
+        const greetingAddress = await greeterContract.logicContractAddress();
+
+        setGreeterContract(greeterContract);
+        setGreeting(greetingAddress);
+
+        window.alert(`Greeter deployed to: ${greeterContract.address}`);
+
+        setGreeterContractAddr(greeterContract.address);
+      } catch (error: any) {
+        window.alert(
+          'Error!' + (error && error.message ? `\n\n${error.message}` : '')
+        );
+      }
+    }
+
+    deployGreeterContract(signer);
+  }
+
+  function handleGreetingChange(event: ChangeEvent<HTMLInputElement>): void {
+    event.preventDefault();
+    setGreetingInput(event.target.value);
+  }
+
+  function handleGreetingSubmit(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+
+    if (!greeterContract) {
+      window.alert('Undefined greeterContract');
+      return;
+    }
+
+    if (!greetingInput) {
+      window.alert('Greeting cannot be empty');
+      return;
+    }
+
+    async function submitGreeting(greeterContract: Contract): Promise<void> {
+      try {
+        const setGreetingTxn = await greeterContract.upgrade(greetingInput);
+
+        await setGreetingTxn.wait();
+
+        const newGreeting = await greeterContract.logicContractAddress();
+        window.alert(`Success!\n\nGreeting is now: ${newGreeting}`);
+
+        if (newGreeting !== greeting) {
+          setGreeting(newGreeting);
+        }
+      } catch (error: any) {
+        window.alert(
+          'Error!' + (error && error.message ? `\n\n${error.message}` : '')
+        );
+      }
+    }
+
+    submitGreeting(greeterContract);
+  }
+
+  return (
+    <>
+      <StyledDeployContractButton
+        disabled={!active || greeterContract ? true : false}
+        style={{
+          cursor: !active || greeterContract ? 'not-allowed' : 'pointer',
+          borderColor: !active || greeterContract ? 'unset' : 'blue'
+        }}
+        onClick={handleDeployContract}
+      >
+        Deploy DutchAuctionNFT Contract
+      </StyledDeployContractButton>
+      <StyledGreetingDiv>
+        <StyledLabel>Proxy Contract addr</StyledLabel>
+        <div>
+          {greeterContractAddr ? (
+            greeterContractAddr
+          ) : (
+            <em>{`<Contract not yet deployed>`}</em>
+          )}
+        </div>
+        {/* empty placeholder div below to provide empty first row, 3rd col div for a 2x3 grid */}
+        <div></div>
+        <StyledLabel>Current logic contract Address</StyledLabel>
+        <div>
+          {greeting ? greeting : <em>{`<Contract not yet deployed>`}</em>}
+        </div>
+        {/* empty placeholder div below to provide empty first row, 3rd col div for a 2x3 grid */}
+        <div></div>
+        <StyledLabel htmlFor="greetingInput">Set new logic contract address</StyledLabel>
+        <StyledInput
+          id="greetingInput"
+          type="text"
+          placeholder={greeting ? '' : '<Contract not yet deployed>'}
+          onChange={handleGreetingChange}
+          style={{ fontStyle: greeting ? 'normal' : 'italic' }}
+        ></StyledInput>
+        <StyledButton
+          disabled={!active || !greeterContract ? true : false}
+          style={{
+            cursor: !active || !greeterContract ? 'not-allowed' : 'pointer',
+            borderColor: !active || !greeterContract ? 'unset' : 'blue'
+          }}
+          onClick={handleGreetingSubmit}
+        >
+          Submit
+        </StyledButton>
+      </StyledGreetingDiv>
+    </>
+  );
+}
